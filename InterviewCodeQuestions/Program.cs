b3b5bef5bc21417a69f6eb8e0f42e09c3ff58cdd
MarcusQuigley <marcusquigley@gmail.com>
2016-12-31 13:20:54 -0500

    Initial checkin Factorial Recursion

---
+++ InterviewCodeQuestions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewCodeQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RecursionTest();
+
+            Console.Read();
+        }
+
+        static void RecursionTest()
+        {
+            Console.WriteLine("Factorial of 5 = {0}", Recursion.Factorial(5));
+
+            Console.WriteLine("Factorial of 4 = {0}", Recursion.Factorial(4));
+            Console.WriteLine("Factorial of 3 = {0}", Recursion.Factorial(3));
+            Console.WriteLine("Factorial of 2 = {0}", Recursion.Factorial(2));
+            Console.WriteLine("Factorial of 1 = {0}", Recursion.Factorial(1));
+
+            Console.WriteLine("End of recursion test {0}", Environment.NewLine);
+        }
+    }
+}
---
+++ InterviewCodeQuestions/Recursion/Recursion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewCodeQuestions
+{
+    static class Recursion
+    {
+
+        public static int Factorial(int value)
+        {
+            if (value == 1)
+                return 1;
+            return value * Factorial(value - 1);
+        }
+    }
+}
